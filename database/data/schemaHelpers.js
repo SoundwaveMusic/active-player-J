71ef31b07e317d1f2766a1f27a389b111ba09645
Lily Carey <lilcare@Lilys-MacBook-Air.local>
2019-10-10 22:49:12 -0700

    Implement songGenerator helper function, test with sampldata

--- database/data/schemaHelpers.js
+++ database/data/schemaHelpers.js
@@ -1,13 +1,30 @@
 const db = require('../index');
 
 module.exports = {
-  songGenerator: () => {
+  songGenerator: (song, cb) => {
     // TBD: write code to generate a single song instance
-  },
-  previousPlays: () => {
-    // TBD: write code to generate a single previous play instance
+    const stmt = `insert into songs (length, timestamp, isliked, songfile, title, artist, album, thumbnail) 
+               values (?, ?, ?, ?, ?, ?, ?, ?)
+              `;
+    let songVals = [
+      song.length,
+      song.timestamp,
+      song.isliked,
+      song.songfile,
+      song.title,
+      song.artist,
+      song.album,
+      song.thumbnail,
+    ];
+    db.queryAsync(stmt, songVals)
+      .then(results => cb(null, results))
+      .catch(err => cb(err));
   },
   upNext: () => {
     // TBD: write code to generate a single upNext play instance
   }
 };
+
+/*
+insert into songs (length, timestamp, isliked, songfile, title, artist, album, thumbnail) values (189, 0, 1, "www.songFile", "A Song I like", "An Artist", "A Great Album", "www.thumbnailLoc");
+*/
--- database/seed.js
+++ database/seed.js
@@ -1,7 +1,21 @@
-const Sequelize = require('sequelize');
 const Promise = require('bluebird');
-const db = require('./index');
+const schema = require('./data/schemaHelpers');
 
+// note default properties: id, timestamp, and isliked
+const sampleSong = {
+  length: 167,
+  songfile: 'www.acoolsongfile.com',
+  title: 'A Cool Song',
+  artist: 'Cool One',
+  album: 'These are Great!',
+  thumbnail: 'www.reallysmallpic.com',
+};
 
+const seedData = () => {
+  schema.songGenerator(sampleSong, (err, results) => {
+    if (err) console.log('err', err);
+    console.log('results', results);
+  });
+};
 
-const 
+seedData();
