9b721ee3e8c14bceeb0f755a70f3653f6703d83b
Lily Carey <lilcare@Lilys-MacBook-Air.local>
2019-10-12 18:43:33 -0700

    Add status codes for response objects

--- server/routeCallbacks.js
+++ server/routeCallbacks.js
@@ -3,22 +3,22 @@ const schema = require('../database/data/schemaHelpers');
 module.exports = {
   getPlay: (req, res) => {
     schema.songGetterAsync(req.params.songid)
-      .then((results) => res.send(results))
-      .catch((err) => res.send(err));
+      .then((results) => res.status(200).send(results))
+      .catch((err) => res.status(404).send(err));
   },
   getPlaylist: (req, res) => {
     schema.playlistGetterAsync(req.params.playlist)
-      .then((results) => res.send(results))
-      .catch((err) => res.send(err));
+      .then((results) => res.status(200).send(results))
+      .catch((err) => res.status(404).send(err));
   },
   timestampEntry: (req, res) => {
     schema.timestampUpdaterAsync(req.params.songid, req.body.timestamp)
-      .then((results) => res.send(results))
-      .catch((err) => res.status(200).send(err));
+      .then((results) => res.status(200).send(results))
+      .catch((err) => res.status(404).send(err));
   },
   playlistEntry: (req, res) => {
     schema.playlistSaverAsync(req.body.songid, req.params.playlist)
       .then((results) => res.status(201).send(results))
-      .catch((err) => res.send(err));
+      .catch((err) => res.status(400).send(err));
   },
 };
