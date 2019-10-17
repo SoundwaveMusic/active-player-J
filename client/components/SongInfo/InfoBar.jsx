8a2c29db580d96a7cbd7d1434bb042a6b422e02a
Lily Carey <lilcare@Lilys-MacBook-Air.local>
2019-10-17 16:17:55 -0700

    Implement MetaData component and grid stylings

--- client/components/SongInfo/InfoBar.jsx
+++ client/components/SongInfo/InfoBar.jsx
@@ -1,28 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import MetaData from './MetaData';
 
 
 function InfoBar({ playerSong }) {
-  const clickAlbum = () => {
-    alert(`Redirecting to the album page for ${playerSong.album}`);
+  const click = (type, value) => {
+    alert(`Redirecting to the ${type} page for ${value}`);
   };
 
   return (
     <div id="infoContainer">
-      <span
-        className="albumArt"
+      <div
+        id="albumArt"
         role="button"
-        onClick={clickAlbum}
-        onKeyDown={clickAlbum}
+        onClick={() => click('album', playerSong.album)}
+        onKeyDown={() => click('album', playerSong.album)}
         tabIndex="-1"
         aria-label="album"
       >
         <img className="albumArt" alt="album artwork" src={playerSong.thumbnail} />
-      </span>
-      <div id="infoGrid">
-        <div className="info" id="artist" />
-        <div className="info" id="title" />
       </div>
+      <div id="infoGrid">
+        <div
+          id="artist"
+          role="button"
+          onClick={() => click('artist', playerSong.artist)}
+          onKeyDown={() => click('artist', playerSong.artist)}
+          tabIndex="-1"
+          aria-label="artist"
+        >
+          {playerSong.artist}
+        </div>
+        <div
+          id="title"
+          role="button"
+          onClick={() => click('song', playerSong.title)}
+          onKeyDown={() => click('song', playerSong.title)}
+          tabIndex="-1"
+          aria-label="song title"
+        >
+          {playerSong.title}
+        </div>
+      </div>
+      <MetaData playerSong={playerSong.isLiked} />
     </div>
   );
 }
@@ -31,6 +51,9 @@ InfoBar.propTypes = {
   playerSong: PropTypes.shape({
     album: PropTypes.string.isRequired,
     thumbnail: PropTypes.string.isRequired,
+    artist: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    isLiked: PropTypes.bool.isRequired,
   }).isRequired,
 };
 
