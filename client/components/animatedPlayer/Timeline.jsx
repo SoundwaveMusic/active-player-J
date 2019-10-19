d0a616df549e3c6805a29b89e986e321ced3add3
Lily Carey <lilcare@Lilys-MacBook-Air.local>
2019-10-18 20:48:44 -0700

    Create newTimestamp helper function for Timeline click handler

--- client/components/animatedPlayer/Timeline.jsx
+++ client/components/animatedPlayer/Timeline.jsx
@@ -4,8 +4,22 @@ import PropTypes from 'prop-types';
 function Timeline({ length, elapsed }) {
   const width = `${(elapsed / length) * 100}%`;
   const animatedStyles = { width };
+
+  const getNewTimestamp = (e) => {
+    // Get the right and left bounds of the timeline element, and click location
+    // To get the scrubLocation: Subtract the left bound from the click location
+    // To get the totalWidth: Subtract the left bound from the right bound
+    // Divide scrubLocation by totalWidth to geth the ratio of the total
+    // Multiply by the song length to get the new timestamp
+    const leftTlineBound = e.target.getBoundingClientRect().left;
+    const rightTlineBound = e.target.getBoundingClientRect().right;
+    const clickLocation = e.clientX;
+    const newTimestamp = ((clickLocation - leftTlineBound) / (rightTlineBound - leftTlineBound)) * length;
+    console.log('new timestamp: ', newTimestamp);
+    return newTimestamp;
+  }
   return (
-    <div id="timelineContainer">
+    <div id="timelineContainer" onClick={getNewTimestamp} >
       <div id="timeline">
         <div className="animatedTimeline" style={animatedStyles} />
       </div>
