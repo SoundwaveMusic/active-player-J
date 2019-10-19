de577933df065232947bc62f623a82fe5d59861e
Lily Carey <lilcare@Lilys-MacBook-Air.local>
2019-10-18 22:43:10 -0700

    Fix a11y rules for Timeline button remove click outline in styles

--- client/components/animatedPlayer/Timeline.jsx
+++ client/components/animatedPlayer/Timeline.jsx
@@ -2,25 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Timeline({ length, elapsed, scrub }) {
+  // Divide elapsed by length to get the progress ratio and multiply by 100%
   const width = `${(elapsed / length) * 100}%`;
   const animatedStyles = { width };
 
   const getNewTimestamp = (e) => {
-    // Get the right and left bounds of the timeline element, and click location
-    // To get the scrubLocation: Subtract the left bound from the click location
-    // To get the totalWidth: Subtract the left bound from the right bound
-    // Divide scrubLocation by totalWidth to geth the ratio of the total
-    // Multiply by the song length to get the new timestamp
     const leftTlineBound = e.target.getBoundingClientRect().left;
     const rightTlineBound = e.target.getBoundingClientRect().right;
     const clickLocation = e.clientX;
+    // To get the scrubLocation: Subtract the left bound from the click location
+    // To get the totalWidth: Subtract the left bound from the right bound
+    // Divide scrubLocation by totalWidth to geth the ratio of the total
     let newTimestamp = ((clickLocation - leftTlineBound) / (rightTlineBound - leftTlineBound));
+    // Multiply by the song length to get the new timestamp
     newTimestamp *= length;
-    console.log('new timestamp: ', newTimestamp);
     scrub(newTimestamp);
-  }
+  };
+
   return (
-    <div id="timelineContainer" onClick={getNewTimestamp} >
+    <div
+      id="timelineContainer"
+      onClick={getNewTimestamp}
+      onKeyDown={getNewTimestamp}
+      role="button"
+      tabIndex="-1"
+      aria-label="song progress bar"
+    >
       <div id="timeline">
         <div className="animatedTimeline" style={animatedStyles} />
       </div>
